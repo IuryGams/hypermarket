5c9ca9548737d25a3fb653a79e0c1e4d9f2016f3
Iury Gama <Iuryempregos@gmail.com>
2024-04-04 00:02:08 -0300

    fix: Bug

--- src/shared/contexts/cartContext.tsx
+++ src/shared/contexts/cartContext.tsx
@@ -3,6 +3,7 @@ import { ProductProps } from "@/types";
 import { Dispatch, SetStateAction, createContext, useContext, useState } from "react";
 import AlgoritmoNotas from "../lib/AlgoritmoTroco";
 import CalculateTotalPrice from "../utils/calculateTotalPrice";
+import CalculateTaxation from "../utils/calculateImpost";
 
 export type ProductContext = {
     products: ProductProps[];
@@ -35,6 +36,7 @@ export function useCartContext() {
 
     const [cash, setCash] = useState<number>(0);
     const fullPrice = CalculateTotalPrice(products);
+    const tax = CalculateTaxation(fullPrice)
 
     function addProductToCart(newProduct: ProductProps) {
         const repetedProduct = products.find( product => product.name === newProduct.name);
@@ -88,7 +90,7 @@ export function useCartContext() {
     }
 
     function Cashier(){
-        const Notas: string[] = AlgoritmoNotas(cash - fullPrice);
+        const Notas: string[] = AlgoritmoNotas(cash - (fullPrice + tax));
         if(Notas.length > 0){
             return Notas;
         }
@@ -105,7 +107,8 @@ export function useCartContext() {
         cash,
         setCash,
         Cashier,
-        fullPrice
+        fullPrice,
+        tax
     }
 }
 
