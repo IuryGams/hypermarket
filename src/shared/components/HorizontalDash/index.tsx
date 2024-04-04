271e7b81c00aa4cc345c02e4a3e9bc6cf574ee77
Iury Gama <Iuryempregos@gmail.com>
2024-04-03 23:05:41 -0300

    feat: create Dash Component

---
+++ src/shared/components/HorizontalDash/index.tsx
@@ -0,0 +1,8 @@
+export default function HorizontalDash(){
+    return <hr style={{
+        width: "100%",
+        height: "2px",
+        backgroundColor: "var(--subtext-color)",
+        border: "none"
+    }}/>
+}
